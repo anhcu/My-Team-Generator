4d356faacb8c233c6e501b8dd17c4cbe4c4f8c92
Hunter <no@no.com>
2021-06-23 10:48:08 -0400

    add engineer, intern, manager

--- index.js
+++ index.js
@@ -2,3 +2,46 @@ const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const inquirer = require('inquirer')
+
+
+const question = [{
+    type: "input",
+    message: "What is your name?",
+    name:"name"
+},{
+    type: "input",
+    message: "What is your ID number?",
+    name:"name"
+},{
+    type: "input",
+    message: "What is your email?",
+    name:"name"
+}]
+
+// function writeToFile(fileName,data){
+
+//     fs.writeFile(fileName, data, function(err) {
+//         console.log(fileName)
+//         console.log(data)
+//         if (err) {
+//             return console.log(err);
+//         } else {
+//             console.log ("Successfully wrote:" + fileName);
+//         }
+//         })
+//     }
+    
+//     function init() {
+//         inquirer.prompt(questions)
+//         .then(function(data) {
+//             writeToFile('test.md', generateMarkdown(data));
+//             console.log(data.License)
+//         })
+//     }
+    
+    
+//     // Function call to initialize app
+//     init();
+    
+    
+    
