831e3c08e97be9540271d093295d0b6aeee1e91d
Hunter <no@no.com>
2021-06-24 10:37:08 -0400

    update index

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const path = require('path');
+const teamArray = [];
+
 
 function teamMemberInput() {
     inquirer
@@ -11,15 +12,15 @@ function teamMemberInput() {
             {
                 type: "input",
                 message: "What is your name?",
-                name:"name"
+                name:"Name"
             },{
                 type: "input",
                 message: "What is your ID number?",
-                name:"name"
+                name:"ID"
             },{
                 type: "input",
                 message: "What is your email?",
-                name:"name"
+                name:"Email"
             },{
                 type: "checkbox",
                 message: "What is your role?(Use spacebar to select)",
@@ -35,6 +36,58 @@ function teamMemberInput() {
                         name: "Github",
                         message: "Enter Github username:"
                     }
-                                    ])
+                ])
+                    .then((roleField) => {
+                        let newMember = new Engineer(data.name, data.id, data.email, roleField.github);
+                        teamArray.push(newMember);
+                        addUser();
+                    })
             }
-        }
+            else if (data.role === "Intern") {
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "school",
+                        message: "What school you're attending"
+                    }
+                ])
+                    .then((roleField) => {
+                        let newMember = new Intern(data.name, data.id, data.email, roleField.github);
+                        teamArray.push(newMember);
+                        addUser();
+                    })
+            }
+            else {
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "office",
+                        message: "What's your office number?"
+                    }
+                ])
+                    .then((roleField) => {
+                        let newMember = new office(data.name, data.id, data.email, roleField.github);
+                        teamArray.push(newMember);
+                        addUser();
+                    })
+            }
+
+        })
+    };
+
+    function addUser() {
+        inquirer.prompt([
+            {
+                type: "yes",
+                message: "Would you like to add a new team member?",
+                name: "newMember"
+            }
+        ]).then((data) =>{
+            if(data.newMember === true) {
+                teamMemberInput();
+            }
+            else {
+                generateHTML();
+            }
+        })
+    }
