4ebe7aef1c5c9f2069313e43e68acfd8409f8816
Hunter <no@no.com>
2021-06-24 10:17:33 -0400

    update engineer input

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const fs = require('fs');
+// const path = require('path');
 
-
-const question = [{
-    type: "input",
-    message: "What is your name?",
-    name:"name"
-},{
-    type: "input",
-    message: "What is your ID number?",
-    name:"name"
-},{
-    type: "input",
-    message: "What is your email?",
-    name:"name"
-}]
-
-// function writeToFile(fileName,data){
-
-//     fs.writeFile(fileName, data, function(err) {
-//         console.log(fileName)
-//         console.log(data)
-//         if (err) {
-//             return console.log(err);
-//         } else {
-//             console.log ("Successfully wrote:" + fileName);
-//         }
-//         })
-//     }
-    
-//     function init() {
-//         inquirer.prompt(questions)
-//         .then(function(data) {
-//             writeToFile('test.md', generateMarkdown(data));
-//             console.log(data.License)
-//         })
-//     }
-    
-    
-//     // Function call to initialize app
-//     init();
-    
-    
-    
+function teamMemberInput() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is your name?",
+                name:"name"
+            },{
+                type: "input",
+                message: "What is your ID number?",
+                name:"name"
+            },{
+                type: "input",
+                message: "What is your email?",
+                name:"name"
+            },{
+                type: "checkbox",
+                message: "What is your role?(Use spacebar to select)",
+                name:"Role",
+                choices: ['Engineer', 'Intern', 'Manager']
+            }
+        ])
+        .then((data) =>{
+            if (data.role === "Engineer") {
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "Github",
+                        message: "Enter Github username:"
+                    }
+                                    ])
+            }
+        }
